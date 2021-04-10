b154f8bac5c1df602044d4ec5a3ecd120d3fa8a6
PuddingPepzi <82250394+PuddingPepzi@users.noreply.github.com>
2021-04-09 23:58:33 -0400

    Publish

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODMwMjgyMDIyMTE0OTUxMjA4.YHEaYw.0sy8lXzsEldY_s0eFRORkSFkshM',
+        prefix: '-',
+        activity: 'being swag and stuff',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
